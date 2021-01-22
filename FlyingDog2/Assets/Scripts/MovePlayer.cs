15d5cbc9417ac3f9b593b25d7fef1db48464e0ba
ignurof <49594591+ignurof@users.noreply.github.com>
2021-01-23 00:24:11 +0100

    comment code & restrict movement to view

---
+++ FlyingDog2/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 6f;
+
+    // Vector2 is like GetAxis
+    private Vector2 direction;
+
+    public void Controls(InputAction.CallbackContext context)
+    {
+        // Move Action is a Vector2 so we set the direction based on values returned from Move action whenever a Move action key is pressed
+        direction = context.ReadValue<Vector2>(); 
+    }
+
+    private void FixedUpdate()
+    {
+        // Restrict movement to game view
+        if (transform.position.x <= -8f)
+            transform.position = new Vector2(-8f, transform.position.y);
+        else if (transform.position.x >= 8f)
+            transform.position = new Vector2(8f, transform.position.y);
+
+        // For example, when we hold D, x is 1, if we hold A, x is -1
+        transform.Translate(direction.x * speed * Time.deltaTime, 0, 0);
+    }
+}
