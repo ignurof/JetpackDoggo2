7172ee5e6d45ca244db6582b19b98be7d5853a45
ignurof <49594591+ignurof@users.noreply.github.com>
2021-01-22 23:58:11 +0100

    player movement using new input system

--- FlyingDog2/Assets/Scripts/Move.cs
+++ FlyingDog2/Assets/Scripts/Move.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 6f;
+
+    // Vector2 is like GetAxis
+    private Vector2 direction;
+
+    public void Controls(InputAction.CallbackContext context)
     {
-        
+        direction = context.ReadValue<Vector2>(); 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        transform.Translate(direction.x * speed * Time.deltaTime, 0, 0);
     }
 }
