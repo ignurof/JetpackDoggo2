6d3d7028cc1f59861e065be940a5f12f4fe6f69b
ignurof <49594591+ignurof@users.noreply.github.com>
2021-01-23 15:50:47 +0100

    background v2

---
+++ FlyingDog2/Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    private bool isStart;
+    public float speed = 6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isStart = false;
+        StartCoroutine(SlowStartTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isStart)
+        {
+            transform.Translate(0, -1 * speed * Time.deltaTime, 0);
+            if (transform.position.y <= 0)
+            {
+                isStart = false;
+            }
+        }
+    }
+
+    IEnumerator SlowStartTimer()
+    {
+        yield return new WaitForSeconds(6);
+        isStart = true;
+    }
+}
