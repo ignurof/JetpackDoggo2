043b4d9986dbc539d4442a9829e436c25b380425
ignurof <49594591+ignurof@users.noreply.github.com>
2021-01-24 18:30:10 +0100

    setup ui & pause functionality

---
+++ FlyingDog2/Assets/Scripts/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public bool isAlive;
+    public GameObject pausePanel;
+
+    private void Start()
+    {
+        isAlive = true;
+        pausePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!isAlive)
+        {
+            // Pause time
+            Time.timeScale = 0;
+            // Activate UI
+            pausePanel.SetActive(true);
+        }
+    }
+}
