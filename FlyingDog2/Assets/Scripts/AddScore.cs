d1fb4a1ce5b988f3813caf29349c6a16cd16c45d
ignurof <49594591+ignurof@users.noreply.github.com>
2021-01-24 18:46:40 +0100

    score: hamburger edition

---
+++ FlyingDog2/Assets/Scripts/AddScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScore : MonoBehaviour
+{
+    public int addAmount;
+
+    // Using this to reset player position, otherwise it goes down from collision
+    private Vector2 oldPos;
+
+    private void Update()
+    {
+        oldPos = transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        transform.position = oldPos;
+        if (collision.gameObject.CompareTag("Hamburger"))
+        {
+            Destroy(collision.gameObject);
+            GameObject.Find("Canvas").GetComponent<UpdateScore>().scoreAmount += 1;
+        }
+    }
+}
