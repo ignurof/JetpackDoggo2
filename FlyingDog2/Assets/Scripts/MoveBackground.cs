1535b8e2759dc05d9a9c99262d9cae7b1a6cf193
ignurof <49594591+ignurof@users.noreply.github.com>
2021-01-25 23:27:03 +0100

    bg movement event

--- FlyingDog2/Assets/Scripts/MoveBackground.cs
+++ FlyingDog2/Assets/Scripts/MoveBackground.cs
@@ -8,11 +8,24 @@ public class MoveBackground : MonoBehaviour
     public float speed = 2f;
     public GameObject prefab;
 
+    // Keep track of scoreCounter on UpdateScore.cs
+    public GameObject scoreObj;
+    private int score;
+
     // Start is called before the first frame update
     void Start()
     {
         isStart = false;
-        StartCoroutine(MoveBGTimer());
+    }
+
+    private void Update()
+    {
+        // Move the obstacle only if X amount of score
+        score = scoreObj.GetComponent<UpdateScore>().scoreAmount;
+        if (score > 10)
+        {
+            StartCoroutine(MoveBGTimer());
+        }
     }
 
     // Update is called once per frame
