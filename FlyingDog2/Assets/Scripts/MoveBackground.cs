92a0f621397a46d681c6fc1054ac5a36e38f2022
ignurof <49594591+ignurof@users.noreply.github.com>
2021-01-25 23:46:50 +0100

    fix bg bug & add dev cheats

--- FlyingDog2/Assets/Scripts/MoveBackground.cs
+++ FlyingDog2/Assets/Scripts/MoveBackground.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MoveBackground : MonoBehaviour
 {
     private bool isStart;
+    private bool isStartTwo;
+
     public float speed = 2f;
     public GameObject prefab;
 
@@ -12,19 +14,27 @@ public class MoveBackground : MonoBehaviour
     public GameObject scoreObj;
     private int score;
 
+    private bool firstCompleted;
+
     // Start is called before the first frame update
     void Start()
     {
         isStart = false;
+        isStartTwo = false;
+        firstCompleted = false;
     }
 
     private void Update()
     {
         // Move the obstacle only if X amount of score
         score = scoreObj.GetComponent<UpdateScore>().scoreAmount;
-        if (score > 10)
+        if (score > 10 && !firstCompleted)
         {
             StartCoroutine(MoveBGTimer());
+        }
+        if(score > 20 && firstCompleted)
+        {
+            StartCoroutine(MoveBGTimerTwo());
         }
     }
 
@@ -36,9 +46,21 @@ public class MoveBackground : MonoBehaviour
             transform.Translate(0, -1 * speed * Time.deltaTime, 0);
             if (transform.position.y <= 0)
             {
+                transform.position = new Vector3(transform.position.x, 0, transform.position.z);
                 isStart = false;
                 // Update obstacle speed
                 prefab.GetComponent<MoveObstacle>().SetSpeed(10f);
+            }
+        }
+
+        if (isStartTwo)
+        {
+            transform.Translate(0, -1 * speed * Time.deltaTime, 0);
+            if(transform.position.y <= -10f)
+            {
+                transform.position = new Vector3(transform.position.x, -10f, transform.position.z);
+                isStartTwo = false;
+                prefab.GetComponent<MoveObstacle>().SetSpeed(15f);
             }
         }
     }
@@ -47,7 +69,12 @@ public class MoveBackground : MonoBehaviour
     {
         yield return new WaitForSeconds(8);
         isStart = true;
+        firstCompleted = true;
     }
 
-
+    IEnumerator MoveBGTimerTwo()
+    {
+        yield return new WaitForSeconds(8);
+        isStartTwo = true;
+    }
 }
