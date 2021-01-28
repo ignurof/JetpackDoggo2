ca46e6801a25389f0359e0b77e115ae4be96b514
ignurof <49594591+ignurof@users.noreply.github.com>
2021-01-28 20:34:26 +0100

    timer functionality & finish menus

--- FlyingDog2/Assets/Scripts/GameOver.cs
+++ FlyingDog2/Assets/Scripts/GameOver.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameOver : MonoBehaviour
 {
     public bool isAlive;
     public GameObject pausePanel;
+
+    private float coolTimer;
 
     private void Start()
     {
@@ -15,12 +18,27 @@ public class GameOver : MonoBehaviour
 
     private void Update()
     {
+        coolTimer += Time.deltaTime;
         if (!isAlive)
         {
             // Pause time
             Time.timeScale = 0;
             // Activate UI
             pausePanel.SetActive(true);
+
+            pausePanel.transform.GetChild(4).GetComponent<TMP_Text>().text = "Time: " + coolTimer;
         }
+    }
+
+    public void QuitGame()
+    {
+        // save any game data here
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
--- FlyingDog2/Assets/Scripts/MainMenu.cs
+++ FlyingDog2/Assets/Scripts/MainMenu.cs
@@ -9,4 +9,16 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+    public void QuitGame()
+    {
+        // save any game data here
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
