874a78c874c220f6d855c04e426ada1d38707654
ignurof <49594591+ignurof@users.noreply.github.com>
2021-08-09 21:16:54 +0200

    Delete FlyingDog2 directory

--- FlyingDog2/Assets/Scripts/SpeedSO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SpeedSO")]
-public class SpeedSO : ScriptableObject
-{
-    public float speed;
-}
